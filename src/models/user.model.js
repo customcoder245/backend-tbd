58b58a66e72ddb2571007b1ff09862b638b61733
Custom coder <customcoder245@gmail.com>
2026-02-11 14:22:42 +0530

    update auth.controller

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -145,7 +145,7 @@ userSchema.methods.generateAccessToken = function () {
   return jwt.sign(
     { userId: this._id, role: this.role, orgName: this.orgName },
     process.env.JWT_SECRET,
-    { expiresIn: "180m" }
+    { expiresIn: "3hr" }
   );
 };
 
