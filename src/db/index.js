a507139174ff901daad26f9bbadae8495c0f66ad
Custom coder <customcoder245@gmail.com>
2026-03-06 18:11:52 +0530

    Fix moogoose connection handling and Vercel cold-start timeouts

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  // If already connected, do not create a new connection or pool
+  if (mongoose.connection.readyState >= 1) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
       maxPoolSize: 10,
